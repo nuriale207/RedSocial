f11c0ff6bbd8e88495c23dc46d76955972a42cd6
Nuria <nurialeb@gmail.com>
2021-12-29 23:26:39 +0100

    Preprocesado

---
+++ Preprocesado.py
@@ -0,0 +1,60 @@
+
+import sys
+#encoding: utf-8
+from statistics import mean
+
+sys.path.append("..")
+#print(sys.path)
+import pandas as pd
+
+if __name__ == "__main__":
+
+    if (len(sys.argv) - 1 != 0):
+        print("This runnable executes the preproccess of a csv file. It "
+              "generates a CSV file with the following columns: Id_A,Id_B,class. ")
+
+    else:
+
+        ##Step1: Load CSV file
+        dfRaw=pd.read_csv("data/train.csv",encoding=  "utf-8")
+        # dfRaw = pd.read_csv("ejemplo.csv",encoding=  "utf-8")
+
+        print(len(dfRaw))
+        # mediaA=mean([dfRaw["A_follower_count"],dfRaw["A_following_count"],dfRaw["A_listed_count"],dfRaw["A_mentions_received"],
+        #             dfRaw["A_retweets_received"],dfRaw["A_mentions_sent"],dfRaw["A_retweets_sent"],dfRaw["A_follower_count"],
+        #             dfRaw["A_posts"],dfRaw["A_network_feature_1"],dfRaw["A_network_feature_2"],dfRaw["A_network_feature_3"]])
+
+
+                # listText=text.split(" ")
+        # print(listText[0])
+        listaTextos=[]
+        iDdic=dict()
+        idIndex=0
+        idA=[]
+        idB=[]
+        clas=[]
+        for i in range(len(dfRaw)):
+
+            mediaA = dfRaw["A_follower_count"][i]+ dfRaw["A_following_count"][i] +dfRaw["A_listed_count"][i]+dfRaw["A_mentions_received"][i]+ dfRaw["A_retweets_received"][i]+dfRaw["A_mentions_sent"][i]+dfRaw["A_retweets_sent"][i]+dfRaw["A_follower_count"][i]+dfRaw["A_posts"][i]+dfRaw["A_network_feature_1"][i]+dfRaw["A_network_feature_2"][i]+dfRaw["A_network_feature_3"][i]
+            mediaA=mediaA/12
+            if(mediaA not in iDdic):
+                iDdic[mediaA]=idIndex
+                idIndex+=1
+
+            mediaB= dfRaw["B_follower_count"][i]+ dfRaw["B_following_count"][i] +dfRaw["B_listed_count"][i]+dfRaw["B_mentions_received"][i]+ dfRaw["B_retweets_received"][i]+dfRaw["B_mentions_sent"][i]+dfRaw["B_retweets_sent"][i]+dfRaw["B_follower_count"][i]+dfRaw["B_posts"][i]+dfRaw["B_network_feature_1"][i]+dfRaw["B_network_feature_2"][i]+dfRaw["B_network_feature_3"][i]
+            mediaB=mediaB/12
+
+            if (mediaB not in iDdic):
+                iDdic[mediaB] = idIndex
+                idIndex += 1
+
+            idA.append(iDdic[mediaA])
+            idB.append(iDdic[mediaB])
+            clas.append(dfRaw["Choice"][i])
+        print(idA)
+        print(idB)
+
+        preprocesseData = {'idA': idA, 'idB': idB,"class":clas}
+        df = pd.DataFrame(preprocesseData)
+
+        df.to_csv(path_or_buf="data/preprocessedTrain.csv", sep=',', encoding="utf-8",index=False)
